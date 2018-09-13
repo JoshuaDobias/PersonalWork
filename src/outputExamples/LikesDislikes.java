20c126cc76a7d9cf4b835ecb791b425a5beb2d95
Joshua Dobias <joshua.a.dobias@gmail.com>
2018-09-13 14:03:52 -0500

    incomplete Likes/Dislike Columns

---
+++ src/outputExamples/LikesDislikes.java
@@ -0,0 +1,13 @@
+package outputExamples;
+
+public class LikesDislikes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String Heading1 = "Likes";
+		String Heading2 = "Dislikes";
+		System.out.printf("%-15s %15s %n", Heading1, Heading2);
+	
+
+	}
+}
