3c671e849e819607566fb937bb39c150eddc6369
Joshua Dobias <joshua.a.dobias@gmail.com>
2018-09-13 13:17:04 -0500

    Full name and age

---
+++ src/outputExamples/NameAge.java
@@ -0,0 +1,12 @@
+package outputExamples;
+
+public class NameAge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Joshua");
+		System.out.println("Dobias");
+		System.out.println(17);
+	}
+
+}
