d48d0939db105578acee2b23a1a76cb5f0c5ab43
Joshua Dobias <joshua.a.dobias@gmail.com>
2018-09-17 14:09:32 -0500

    Finished Likes/Dislikes

--- src/outputExamples/LikesDislikes.java
+++ src/outputExamples/LikesDislikes.java
@@ -7,6 +7,11 @@ public class LikesDislikes {
 		String Heading1 = "Likes";
 		String Heading2 = "Dislikes";
 		System.out.printf("%-15s %15s %n", Heading1, Heading2);
+		System.out.printf("%-15s %15s %n", "Food", "School");
+		System.out.printf("%-15s %15s %n", "Sleep", "Work");
+		System.out.printf("%-15s %15s %n", "Friends", "sickness");
+		System.out.printf("%-15s %15s %n", "Games", "Lonliness");
+		System.out.printf("%-15s %15s %n", "Computers", "losing money");
 	
 
 	}
