963632e22688dbbcdb8543c74b14798aa2cbc4db
Joshua Dobias <joshua.a.dobias@gmail.com>
2018-10-03 14:57:57 -0500

    Room program complete

--- src/oOP/Test.java
+++ src/oOP/Test.java
@@ -3,11 +3,13 @@ package oOP;
 public class Test {
 
 	public static void main(String[] args) {
-		Car maz=new Car();
-		maz.sayOdometer();
-		maz.sayFlat();
-		maz.sayThirsty();
-
+		Room closet=new Room();
+		Room kitchen=new Room();
+		kitchen=closet;
+		Room basement=new Room();
+		closet=basement;
+		closet.area();
+				
 	}
 
 }
